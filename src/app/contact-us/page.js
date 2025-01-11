4f5041ec84be0a829d9ed5883b3820e7a7ee98d1
maj.jinnah <maj.jinnah1999@gmail.com>
2025-01-12 00:22:20 +0600

    contract and about us page is complate

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -1,8 +1,35 @@
+import ContactForm from "@/components/contact/ContactForm";
+import ContactInfo from "@/components/contact/ContactInfo";
+import GoogleMap from "@/components/contact/GoogleMap";
+
 function ContactUs() {
   return (
-    <div>
-      <p>ContactUs</p>
-    </div>
+    <>
+      <div className=" bg-[#014cda]">
+        <div className="container mx-auto max-w-[1390px] px-2 text-white text-center space-y-4 py-8">
+          <h1 className="text-4xl font-bold">Contact Us</h1>
+          <p className="text-xl w-2/3 mx-auto ">Web hosting made easy & affordable, choose a fine-tuned web hosting services solution for successful personal and business websites.</p>
+        </div>
+      </div>
+      <div className="container mx-auto max-w-[1390px] px-2">
+        <ContactInfo />
+        <ContactForm />
+      </div>
+
+      {/* Google img or google map */}
+      <div>
+        <GoogleMap />
+      </div>
+
+      <div className="mb-10">
+        <h1 className="bg-[#EBF3FF] text-[#204674] text-center py-2 font-semibold text-xl border-t border-b border-[#80a4d9]">Location Of Our Clients</h1>
+        
+        {/* carusel add korte hobe */}
+        <div className="container mx-auto max-w-[1390px] px-2">
+          Country flag Carusel
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/about/HelpSection .js
@@ -0,0 +1,33 @@
+import { Ticket } from "lucide-react";
+import { PhoneCall } from "lucide-react";
+import { Phone } from "lucide-react";
+
+const HelpSection = () => {
+    return (
+      <div className={`bg-[url('/background.jpg')] bg-cover bg-center text-white rounded-lg p-10 relative overflow-hidden`}>
+        <h1 className="text-3xl font-semibold text-center mb-4">
+          Need Help? We Are Here To Help You
+        </h1>
+        <p className="text-center text-lg mb-8">
+          Please feel free to consult with one of our experts, who will thoroughly evaluate your specific requirements and provide a tailored proposal that perfectly aligns with your company’s unique needs and realities.
+        </p>
+        <div className="flex flex-wrap justify-center gap-4">
+          <a href="tel:+88 01823 662233" className="flex items-center gap-2 bg-blue-800 px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+          <Phone size={16} /> Call US
+          </a>
+          <button className="flex items-center gap-2 bg-blue-800 px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+            💬 Live Chat
+          </button>
+          <button className="flex items-center gap-2 bg-blue-800 px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+          <PhoneCall size={16} /> Call Back
+          </button>
+          <button className="flex items-center gap-2 bg-blue-800 px-6 py-3 rounded-lg hover:bg-blue-700 transition">
+          <Ticket size={16} /> Open Ticket
+          </button>
+        </div>
+      </div>
+    );
+  };
+  
+  export default HelpSection;
+  
