fc1e0ffeeedf325994740c9f606104fc20f49fb4
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-01-20 21:37:58 +0600

    respnsive navbar add

--- src/components/premiumHosting/banner/Banner.js
+++ src/components/premiumHosting/banner/Banner.js
@@ -3,35 +3,35 @@ import Image from "next/image";
 function Banner() {
   return (
     <section>
-      <div className="container mx-auto px-3 lg:px-6 lg:flex items-center gap-4 justify-center sm:justify-between py-6">
-        <div className="w-full lg:w-3/4 flex flex-col sm:flex-row items-center gap-2 text-center sm:text-start">
+      <div className="container mx-auto px-3 lg:px-6 flex flex-wrap items-center gap-4 justify-center sm:justify-between py-6">
+        <div className="w-full lg:w-3/4 flex flex-col sm:flex-row items-center sm:items-start gap-4 text-center sm:text-start">
           <Image
             src={"/speed.png"}
             width={400}
             height={400}
-            alt=""
-            className="w-56 h-56 object-contain"
+            alt="Speed Guarantee"
+            className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 object-contain"
           />
           <div>
-            <h2 className="text-3xl">
+            <h2 className="text-xl sm:text-2xl md:text-3xl font-semibold">
               Premium Shared Web Hosting in Bangladesh
             </h2>
-            <p className="leading-7 mt-4">
+            <p className="leading-6 sm:leading-7 text-sm sm:text-base mt-4">
               Premium web hosting is one of the most popular web hosting in
               Bangladesh. Due to the massive advancement of digitalization and
               e-commerce company expansion, people are looking for the best web
               hosting for their website. In this hosting package, we are
-              offering the most featured enriched premium web hosting.
+              offering the most feature-enriched premium web hosting.
             </p>
           </div>
         </div>
-        <div className="w-full sm:w-auto mx-auto">
+        <div className="w-auto mx-auto">
           <Image
             src={"/money-back-guarantee-1.png"}
             width={400}
             height={400}
-            alt=""
-            className="w-56 h-56 object-contain"
+            alt="Money Back Guarantee"
+            className="w-40 h-40 sm:w-48 sm:h-48 md:w-56 md:h-56 object-contain"
           />
         </div>
       </div>
