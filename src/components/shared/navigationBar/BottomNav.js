7838aee85e884e3b0362f86f518de345dfc06dfc
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-01-21 18:03:54 +0600

    sub nav add

--- src/components/shared/navigationBar/BottomNav.js
+++ src/components/shared/navigationBar/BottomNav.js
@@ -5,32 +5,33 @@ import { usePathname } from "next/navigation";
 
 function BottomNav({ navData }) {
   const pathname = usePathname();
+
   return (
     <nav>
+      {/* Main navigation container */}
       <div className="relative container mx-auto px-3 lg:px-6 w-full hidden 2xl:flex items-center justify-center pt-4 transition-all">
         {navData.map((item, index) => (
           <div
             key={index}
-            className={`relative capitalize border px-6 py-2 group 
-             
-            `}
+            className="relative capitalize border px-6 py-2 group z-50"
           >
             <Link
               href={item.url}
-              className={` ${
+              className={`${
                 pathname === item?.url
-                  ? "border-b-4 border-b-[#0d6efd] pb-2"
+                  ? "border-b-4 border-b-[#0d6efd] pb-2 "
                   : null
               }`}
             >
               {item.name}
             </Link>
 
-            {/* {item.info && (
+            {/* Subnav container */}
+            {item.info && (
               <div
-                className={`absolute left-0 top-full w-full opacity-0 group-hover:opacity-100 bg-white shadow-lg p-4 rounded transition-all transform group-hover:translate-y-2 z-50`}
+                className={`absolute left-0 top-full -mt-1.5 w-max bg-white shadow-lg p-4 rounded transition-all transform group-hover:translate-y-2 z-40 hidden group-hover:block`}
               >
-                <div className="max-w-screen mx-auto grid ">
+                <div className="max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                   {item.info.map((subItem, subIndex) => (
                     <Link
                       key={subIndex}
@@ -43,7 +44,7 @@ function BottomNav({ navData }) {
                   ))}
                 </div>
               </div>
-            )} */}
+            )}
           </div>
         ))}
       </div>
