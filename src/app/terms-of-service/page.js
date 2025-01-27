8a31656f95cc59491fe8c4bcc2861ba8fef081a4
mmrecoybot <contact.mustafizur@gmail.com>
2025-01-28 00:25:41 +0600

    content chage

--- src/app/terms-of-service/page.js
+++ src/app/terms-of-service/page.js
@@ -1,9 +1,238 @@
-function TermsOfService() {
+import Image from "next/image";
+import Link from "next/link";
+
+const termsofServices = () => {
   return (
-    <div className="container mx-auto px-3 lg:px-6">
-      <p>TermsOfService</p>
+    <div className="max-w-[1452px] mx-auto p-6">
+      <div>
+         <Image
+            src={"/services/terms_of_services.png"}
+            alt="about_us_Bg"
+            width={2000}
+            height={2000}
+            className="w-full h-64 md:h-80 object-fill"
+         />
+      </div>
+      <h1 className="text-3xl font-bold mb-4 mt-8 text-[#46494c]">Terms of Service</h1>
+
+      <p className="mb-6 text-[#686e78]">This Service Contract (&quot;Contract&quot;) is between Dinisoft (referred to as &quot;We,&quot; &quot;Us,&quot; or &quot;Our&quot;) and the user of Dinisoft services (referred to as &quot;You,&quot; &quot;Your,&quot; &quot;Reseller,&quot; &quot;Client,&quot; or &quot;Customer&quot;). This Contract encompasses the terms and conditions that govern the use of Dinisoft services. All services provided by Dinisoft may be used for lawful purposes only. Transmission, storage or presentation of any information, data or material in violation of any United States federal, state or city law and Bangladesh Govt law is prohibited. This includes, but is not limited to: copyrighted material, material we judge to be threatening or obscene, or material protected by trade secret and other statute. The subscriber agrees to indemnify and hold harmless Dinisoft from any claims resulting from the use of service which damages the subscriber or any other party. Prohibited are sites that promote any illegal activity or present content that may be damaging to Dinisoft servers, or any other server on the Internet. Links to such materials are also prohibited.</p>
+
+      <section className="mb-8">
+        <h5 className="text-2xl font-semibold mb-2 text-[#46494c]">Unacceptable content or links for shared hosting, windows hosting, Reseller, Master Reseller, Cloud Servers and dedicated servers:
+        </h5>
+        <p className="mb-4 text-[#686e78]">
+        This privacy policy (statement) covers all websites that are operated by Dinisoft. Because Dinisoft wants to demonstrate its commitment to our users privacy, we have agreed to disclose our information practices.</p>
+        <p className="mb-4">
+
+        </p>
+      </section>
+
+      <section className="mb-8 text-[#686e78]">
+        <ul className="list-disc list-inside mb-4 text-[#686e78]">
+          <li>Topsites</li>
+          <li>IRC Scripts/Bots</li>
+          <li>
+          Proxy Scripts/Anonymizers
+          </li>
+          <li>Pirated Software/Warez</li>
+          <li>IP Scanners</li>
+          <li>Bruteforce Programs/Scripts/Applications</li>
+          <li>Mail Bombers/Spam Scripts</li>
+          <li>Potential harm to minors is strictly forbidden, including but not limited to child pornography or content perceived to be child pornography (Lolita):</li>
+          <li>Any site found to host child pornography or linking to child pornography will be suspended immediately without notice.</li>
+          <li>File Dump/Mirror Scripts (similar to rapidshare)</li>
+          <li>Commercial Audio Streaming (more than one or two streams)</li>
+          <li>Escrow/Bank Debentures</li>
+          <li>High-Yield Interest Programs (HYIP) or Related Sites</li>
+          <li>Investment Sites (FOREX, E-Gold Exchange, Second Life/Linden Exchange, Ponzi, MLM/Pyramid Scheme)</li>
+          <li>Sale of any controlled substance without prior proof of appropriate permit(s)</li>
+          <li>Prime Banks Programs</li>
+          <li>Lottery Sites</li>
+          <li></li>
+          <li>MUDs/RPGs/PBBGs</li>
+          <li>Hateful/Racist/Harassment oriented sites</li>
+          <li>Hacker focused sites/archives/programs</li>
+          <li>Sites promoting illegal activities</li>
+          <li>Forums and/or websites that distribute or link to warez/pirated/illegal content</li>
+          <li>Bank Debentures/Bank Debenture Trading Programs</li>
+          <li>Fraudulent Sites (Including, but not limited to sites listed at aa419.org & escrow-fraud.com)</li>
+          <li>Mailer Pro</li>
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Delivery of Services</h2>
+        <p className="mb-4 text-[#686e78]">
+        Upon the successful completion of payment, You will receive an automated receipt from our systems. It is advisable to retain a copy of this receipt for future reference. Your unique customer username will be provided on this receipt, which You should use for any inquiries or correspondence with Dinisoft.
+        Dinisoft aims to promptly deliver all services after signup completion. In the event of any delays, You will be notified via email or telephone, or both. Payment will be deducted upon service delivery.
+        Orders are delivered electronically via email. For any order-related questions, please contact us at <Link className="text-[#04444]" href={"mailto:support@dinisoftbd.com"}>support@dinisoftbd.com</Link>.
+        Our customer support operates 24/7, 365 days a year. We typically respond within one hour of receiving your inquiry.
+        Please ensure that you are aware of any export or import restrictions in your country for the products and services available on this site.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">General Terms and Policies</h2>
+        <p className="mb-4 text-[#686e78]">
+        The Service is provided to authorized individuals or organizations (referred to as &quot;Subscriber&quot; or &quot;You&quot;). Any use of the service is subject to the restrictions listed below. By using the Service, You agree to be bound by all these Terms and Policies. If you do not agree to them, you must immediately cancel your account and may not use or attempt to use the Service thereafter.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Coverage</h2>
+        <p className="mb-4 text-[#686e78]">
+        If You are an individual Subscriber, these Terms and Policies apply to all persons who gain access through your account. If You are a commercial Subscriber, these Terms and Policies apply to all your employees, agents, and/or customers. In either instance, a violation of these Terms and Policies by anyone using your account will be treated as a violation by You.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Prices and Charges</h2>
+        <p className="mb-4 text-[#686e78]">
+        Dinisoft provides the use of control panels free of charge, but any charges incurred due to misuse or abuse on your part may be passed on to You.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Term</h2>
+        <p className="mb-4 text-[#686e78]">
+        The Service is provided for as long as you wish to use it; however, Dinisoft reserves the right to terminate the service if it is used in a way that violates the terms of use.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Backups</h2>
+        <p className="mb-4 text-[#686e78]">
+        Dinisoft performs daily backups, but these backups are not guaranteed. They are strictly for our use in case of server failures. We recommend that clients also back up their data to their personal computers or storage servers. Clients are responsible for their own data. A $50 one-time fee is charged for backup restoration.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Suspended or Terminated Account Backups</h2>
+        <p className="mb-4 text-[#686e78]">
+        If you need backup for a suspended account, you need to pay a $50 one-time fee. We keep terminated account backups for 15 days only. The restoration fee is $50 one-time, depending on backup availability.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Grounds for Suspension and Termination</h2>
+        <p className="mb-4 text-[#686e78]">
+        You agree to comply with these Terms and Policies. Any violation may lead to suspension or termination of your account. Grounds for suspension or termination include, but are not limited to:
+        </p>
+
+        <ul className="list-disc list-inside mb-4 text-[#686e78]">
+          <li>Violation of applicable statutes, laws, court orders, regulations, or treaties.</li>
+          <li>Hosting websites dedicated to pornography or adult content.</li>
+          <li>Abusing or violating the privacy or property rights of others, including sending unsolicited bulk email (&quot;spamming&quot;).</li>
+          <li>Using your hosting as a file or image hosting service.</li>
+          <li>Attempting to break security or access unauthorized accounts, messages, or files.</li>
+          <li>Using your account as a backup service.</li>
+          <li>Forgery or misrepresentation of headers, addresses, or other identification.</li>
+          <li>Uploading, linking to, or storing pirated software.</li>
+          <li>Assisting in or directly distributing copyrighted material.</li>
+          <li>Excessive use of the Service, limiting bandwidth available to others.</li>
+          <li>Providing false contact details.</li>
+          <li>Operating server programs without authorization.</li>
+          <li>Unauthorized relays through third-party systems.</li>
+          <li>Attempting to interfere with or deny service to any user or host on the Internet.</li>
+          <li>Mail bombing.</li>
+          <li>Hosting phishing content.</li>
+          <li>Furnishing false data on your sign-up form.</li>
+          <li>Chargebacks on transactions.</li>
+          <li>Engaging in harassment.</li>
+          <li>Engaging in denial-of-service attacks.</li>
+          <li>Forging or misrepresenting identity or location.</li>
+          <li>Using the Service to add addresses to any mailing list without consent.</li>
+          <li>Canceling, superseding, or interfering with email other than your own.</li>
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Security</h2>
+        <p className="mb-4 text-[#686e78]">
+        You agree not to access or attempt to access private areas of the Service. You agree to notify Dinisoft immediately if you become aware of unauthorized use of your account or any breach of security on the Service.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Intellectual Property</h2>
+        <p className="mb-4 text-[#686e78]">Dinisoft does not undertake to examine or review messages, files, or other materials on the Service. Complaints regarding alleged copyright infringement can be sent to abuse@dinisoftbd.com. We will promptly address these complaints, but we do not assume responsibility for the accuracy of third-party materials. You understand and agree that you are solely responsible for your postings on the Internet and agree to indemnify Dinisoft against any claims arising from your postings.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Liability</h2>
+        <p className="mb-4 text-[#686e78]">Your use of the Service is at your own risk. Except for information, products, or services explicitly provided by Dinisoft, neither Dinisoft nor its affiliates control, provide, operate, or are in any way responsible for information, products, or services accessible through the Service. Dinisoft does not endorse third-party materials and is not responsible for any loss or damage caused by your use of such materials. You are solely responsible for your postings and agree to indemnify Dinisoft and its employees, customers, and subscribers from any claims arising from your postings.
+        If you use the Service for voice communications, Dinisoft is not acting as a telecommunications carrier or telephone service provider. There is no representation made by Dinisoft regarding the suitability of the Service for such use. All risks related to connection, transmission quality, and accuracy of communications are solely on You. Dinisoft has no liability for the failure or quality of such use of the Service.
+        You agree to be liable for any damages or loss of service resulting from spamming or other violations. These damages may include system shut-downs, retaliatory attacks, loss of peering arrangements, and more. Dinisoft may pursue such claims in Court.
+        In no event will Dinisoft have financial liability to You.</p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Warranty</h2>
+        <p className="mb-4 text-[#686e78]">
+          Dinisoft makes no express or implied warranties regarding the service, including warranties of title, non-infringement, merchantability, or fitness for a particular purpose. No oral advice or information given by Dinisoft or its employees shall alter this section.
+          The service and any software provided to you are provided on an &quot;as is, as available&quot; basis; Dinisoft does not warrant they will be error-free or uninterrupted. Dinisoft does not guarantee the security or backup of your data.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">VPS CPU Usages</h2>
+        <p className="mb-4 text-[#686e78]">
+        Clients agree not to run processes on their VPS that create a CPU load above 0.9 on the VPS for an extended period or which cause a high CPU load on the host node. Disruptive CPU usage may result in a reboot, shutdown, and/or suspension of the VPS.
+        </p>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">VPN/Proxy Usage Policy for VPS</h2>
+        <p className="mb-4 text-[#686e78]">
+        We want to make it clear that the use of VPNs (Virtual Private Networks) and proxy servers on our VPS services is not allowed unless expressly permitted in a separate agreement with the Company. This policy is in place to ensure the security and integrity of our network and to prevent any misuse of our services. We strictly prohibit activities such as bypassing security measures, engaging in illegal activities, network abuse, and email abuse through VPNs or proxy servers on our VPS. We take violations of this policy seriously and may take appropriate action, including suspension or termination of VPS services, as well as legal action if necessary. It is important for all users to review and adhere to this policy to ensure compliance with our terms and conditions.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Content</h2>
+        <p className="mb-4 text-[#686e78]">
+        You understand and agree that information and access available through the Service may include controversial, sexually explicit, or other material that may be offensive to you or users for whom you are responsible. Dinisoft is not responsible for or in control of such materials, and You are responsible for using screening software or other methods to limit access to material You may find objectionable.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Disputes</h2>
+        <p className="mb-4 text-[#686e78]">
+        Any dispute between You and Dinisoft arising from your use of the Service or your subscription will be submitted to binding arbitration under Bangladeshi law and International Law.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Governing Law
+        </h2>
+        <p className="mb-4 text-[#686e78]">
+        These Terms and Policies are governed by Bangladeshi laws and International Laws.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Cancellation Procedure</h2>
+        <p className="mb-4 text-[#686e78]">Cancellation requests must be submitted through the client area. Click the icon next to the package you wish to cancel and then press &apos;Request Cancellation.&apos; You must complete and submit the form.
+          Immediate cancellation means your service will be terminated within 24 hours of submitting the form.
+          You are responsible for ensuring that any PayPal subscriptions are canceled.
+          No other forms of cancellation are accepted.
+          Cancellations must be submitted before the invoice for that billing cycle is due. Failure to do so will result in the invoice becoming due. Cancellation requests can only be submitted if the account has no overdue invoices.
+          Dedicated server cancellations must be submitted a minimum of 7 days prior to the renewal date, or you may be charged for the next billing period.
+        </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Discount Vouchers</h2>
+        <p className="mb-4 text-[#686e78]">
+        Only one voucher can be used per transaction. If the order value is lower than the voucher value, the remaining amount is lost. Vouchers are restricted to one per customer and can be redeemed against Shared, Reseller, or Dedicated services. </p>
+      </section>
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Account Payments</h2>
+        <p className="mb-4 text-[#686e78]">
+        For accounts with past-due/overdue invoices:</p>
+        <ul className="list-disc list-inside mb-4 text-[#686e78]">
+          <li>1-3 Days: No action taken.</li>
+          <li>4 Days: Accounts are suspended.</li>
+          <li>7 Days: Accounts are terminated.</li>
+          <li>Dedicated servers are suspended as soon as an invoice is overdue.</li>
+        </ul>
+      </section>
+
+      <section className="mb-8">
+        <h2 className="text-2xl font-semibold mb-2 text-[#686e78]">Amendments</h2>
+        <p className="mb-4 text-[#686e78]">
+        These Terms and Policies may be amended at any time by Dinisoft, with amendments posted on the terms of use section of the Service. Your continued use of the Service after such amendments will be considered consent to the updated Terms and Policies. If you do not wish to continue using the Service due to amended Terms and Policies, You may terminate your use of the Service by notifying Dinisoft.</p>
+      </section>
+
     </div>
   );
-}
+};
 
-export default TermsOfService;
+export default termsofServices;
